80ceb2ef836d49b9c174cba4009a212996d77075
Ricky Mormor <rickymormor1@gmail.com>
2022-01-20 08:28:13 +0000

    Add routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import Calculator from './components/Calculator';
 
 class App extends React.Component {
@@ -10,7 +11,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <Calculator />
+      <>
+        <Navbar />
+        <Calculator />
+      </>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>Math Magicians</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/calculator">Calculator</Link>
+        <Link to="/about">Quote</Link>
+      </nav>
+    </div>
+  );
+}
