c1413a2d570b8e79e1caa6e931718f1c6a8adb74
Ricky Mormor <rickymormor1@gmail.com>
2022-01-20 13:13:27 +0000

    Add style rules to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
+import './Footer.css';
+
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>@2022</p>
+        <p>@2022 Ricky Mormor</p>
       </div>
     </footer>
   );
